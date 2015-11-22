9ba856bfa37852e2293346c90fe3940a82ca41fa
kyan215 <kevinyan215@gmail.com>
2015-11-21 16:30:57 -0800

    added sql table to gui

--- WelcomeScreen.java
+++ WelcomeScreen.java
@@ -3,31 +3,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
 
 public class WelcomeScreen {
 	
 	private JFrame frame;
 	private JPanel mainPanel;
 	
+	//JDBC stuff
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	private String s;
+	
 	public WelcomeScreen(){
+		
+		//initialize jdbc variables
+		 con = null;
+		 st = null;
+		 rs = null;
+		 s = "";
+		 
+		 try {
+		       Class.forName("com.mysql.jdbc.Driver").newInstance();
+		 } 
+		 catch (Exception ex) {
+		       // handle the error
+		   	System.out.println("JDBC driver not found");
+		 }
+		   
+		//connect to mysql database
+		 try{
+		       con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbtest", "root", "Gohardorgohome1!");
+		       
+		 }
+		 catch (SQLException ex) {
+	            // handle any errors
+	            System.out.println("SQLException: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("VendorError: " + ex.getErrorCode());
+	     }	
+	      
+		//initialize GUI
+		 
 		//most outer layouts
 		//order of gui layouts. frame, mainPanel (BorderLayout), addPanel/deletePanel/updatePanel (BoxLayouts)
 		frame = new JFrame();
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		mainPanel.setPreferredSize(new Dimension(640, 300));
+		
 		
 		//welcome text on top 
 		JLabel welcomeLabel = new JLabel("Welcome Screen", SwingConstants.CENTER);
 		welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		mainPanel.add(welcomeLabel, BorderLayout.PAGE_START);
 		
+		
+		//calls the methods that adds the other gui
 		displayAddPanel();
 		displayDeletePanel();
 		displayUpdatePanel();
+		displayTabel();
 
-		JButton search = new JButton("Search");
-		mainPanel.add(search, BorderLayout.PAGE_END);
 		
 		frame.add(mainPanel);
 		frame.pack();
@@ -101,6 +144,58 @@ public class WelcomeScreen {
 		updateLogin.setAlignmentX(Component.CENTER_ALIGNMENT);	
 		
 		mainPanel.add(updatePanel, BorderLayout.LINE_END);
+	}
+	
+	public void displayTabel(){
+		//connect to mysql database
+		   try{
+	
+		       st = con.createStatement();
+		       s = "select * from Users";
+		       rs = st.executeQuery(s);
+		       ResultSetMetaData rsmt = rs.getMetaData();
+		       int c = rsmt.getColumnCount();
+		       Vector column = new Vector(c);
+		       for(int i = 1; i <= c; i++)
+		       {
+		           column.add(rsmt.getColumnName(i));
+		       }
+		       Vector data = new Vector();
+		       Vector row = new Vector();
+		       while(rs.next())
+		       {
+		           row = new Vector(c);
+		           for(int i = 1; i <= c; i++){
+		               row.add(rs.getString(i));
+		           }
+		           data.add(row);
+		       }
+		       
+		JPanel bottomPanel = new JPanel(); //holds the table and search button
+		JTable table = new JTable(data,column); //creates the table with data
+		JScrollPane jsp = new JScrollPane(table); //scroll for table
+		JButton search = new JButton("Search");
+		
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.PAGE_AXIS));
+	    bottomPanel.add(jsp);
+	  	bottomPanel.add(search);
+	  	
+	  	
+		mainPanel.add(bottomPanel, BorderLayout.PAGE_END);
+		   
+		}
+	    catch(Exception e){
+	        JOptionPane.showMessageDialog(null, "method error");
+	    }finally{
+	        try{
+	        st.close();
+	        rs.close();
+	        con.close();
+	        }catch(Exception e){
+	            JOptionPane.showMessageDialog(null, "method  error2");
+	        }
+	    }   
+		//
 	}
 
 }
